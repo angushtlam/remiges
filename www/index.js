d74bd3654cbd46ffe42000d4617fc43d8c79cd7a
angus <angushtlam@gmail.com>
2018-09-02 17:20:12 -0400

    Implemented Columns

--- www/index.js
+++ www/index.js
@@ -1,25 +1,45 @@
 import React from 'react'
 import {render} from 'react-dom'
-import {Header, Link, Paragraph, Screen, Subheader} from 'remiges'
+import {
+  Column,
+  Columns,
+  Header,
+  Link,
+  Paragraph,
+  Screen,
+  Subheader,
+} from 'remiges'
 
 const App = () => (
   <Screen>
-    <Header>Remiges</Header>
-    <Subheader>A React UI Component library</Subheader>
-    <Paragraph>
-      Remiges is a React UI Component library built by{' '}
-      <Link to="https://github.com/angusburg" target="_blank">
-        @angusburg
-      </Link>
-      . This is a library created for my projects.
-    </Paragraph>
-    <Subheader>Why?</Subheader>
-    <Paragraph>
-      I prototype on a lot of React-based frontend projects that end up using a
-      lot of the same styling. The worst part is to keep track of all the
-      incremental improvements on the styling from project to project. So, for
-      my sake, I'm going to create one library to keep all these changes.
-    </Paragraph>
+    <Columns>
+      <Column width={12}>
+        <Paragraph>
+          <Link to="https://github.com/angusburg/remiges" target="_blank">
+            Fork me on GitHub!
+          </Link>
+        </Paragraph>
+      </Column>
+      <Column>
+        <Header>Remiges</Header>
+        <Subheader>A React UI Component library</Subheader>
+        <Paragraph>
+          Remiges is a React UI Component library built by{' '}
+          <Link to="https://github.com/angusburg" target="_blank">
+            @angusburg
+          </Link>
+          . This is a library created for my projects.
+        </Paragraph>
+        <Subheader>Why?</Subheader>
+        <Paragraph>
+          I prototype on a lot of React-based frontend projects that end up
+          using a lot of the same styling. The worst part is to keep track of
+          all the incremental improvements on the styling from project to
+          project. So, for my sake, I'm going to create one library to keep all
+          these changes.
+        </Paragraph>
+      </Column>
+    </Columns>
   </Screen>
 )
 
--- www/webpack.config.js
+++ www/webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
+  mode: process.env.NODE_ENV || 'development',
   entry: './www/index.js',
   output: {
     path: path.resolve(__dirname, '..', 'dist', 'www'),
